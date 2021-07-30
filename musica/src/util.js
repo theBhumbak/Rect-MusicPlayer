655d45307cfb99a2cb71398058b7277c6652a95d
theBhumbak <skbhumbak@gmail.com>
2021-07-30 19:34:52 +0530

    util with all music data- song art colors artist title

---
+++ musica/src/util.js
@@ -0,0 +1,65 @@
+import { uuid } from "uuidv4";
+function chillHop() {
+  return [
+    {
+      name: "What If I Told You",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/07/935da7886600df5eeb2d3b13b12cf27ee8c6c700-1024x1024.jpg",
+      artist: "Juan Rios",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=21649",
+      color: ["#fed596", "#4e1c3c"],
+      id: uuid(),
+      active: true,
+    },
+    {
+      name: "Viatecture",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/06/23b2ff959731b56ea8545828b462311e8b1a2bcc-1024x1024.jpg",
+      artist: "Leavv, Makzo",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=21649",
+      color: ["#bb9ed6", "#c8ca3b"],
+      id: uuid(),
+      active: true,
+    },
+    {
+      name: "Dawn",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/05/732128e1da8fd5f6292ffc1926a2ea840a54f4d0-1024x1024.jpg",
+      artist: "Toonorth",
+      audio: "https://chillhop.com/releases/toonorth-aftersome/",
+      color: ["#9c9efb", "#010101"],
+      id: uuid(),
+      active: true,
+    },
+    {
+      name: "What If I Told You",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/07/935da7886600df5eeb2d3b13b12cf27ee8c6c700-1024x1024.jpg",
+      artist: "Juan Rios",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=21649",
+      color: ["#fed596", "#4e1c3c"],
+      id: uuid(),
+      active: true,
+    },
+    {
+      name: "Viatecture",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/06/23b2ff959731b56ea8545828b462311e8b1a2bcc-1024x1024.jpg",
+      artist: "Leavv, Makzo",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=21649",
+      color: ["#bb9ed6", "#c8ca3b"],
+      id: uuid(),
+      active: true,
+    },
+    {
+      name: "Dawn",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/05/732128e1da8fd5f6292ffc1926a2ea840a54f4d0-1024x1024.jpg",
+      artist: "Toonorth",
+      audio: "https://chillhop.com/releases/toonorth-aftersome/",
+      color: ["#9c9efb", "#010101"],
+      id: uuid(),
+      active: true,
+    },
+  ];
+}
